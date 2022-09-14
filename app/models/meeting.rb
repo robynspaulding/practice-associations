cfae52ff67e994c1da1ef14c6c1fc4afd88df77a
RobynSpaulding <robynm27@gmail.com>
2022-09-13 21:09:59 -0700

    adds validations for meeting and speaker model attributes

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,9 @@
 class Meeting < ApplicationRecord
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :location, presence: true 
+  validates :time, presence: true
   has_many :events
   has_many :speakers, through: :events 
+
 end
