3fdf72855a6f2ebd81e879946dd60fa8751eadda
RobynSpaulding <robynm27@gmail.com>
2022-09-13 20:53:23 -0700

    adds three speaker instances and two meeting instances, via rails c (also copied in my seeds.rb)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Speaker.create(
+  first_name: "Sam",
+  last_name: "Harris",
+  email: "sh@test.com"
+)
+
+Speaker.create(
+  first_name: "Jen",
+  last_name: "Thomas",
+  email: "jt@test.com"
+)
+
+Speaker.create(
+  first_name: "Sara",
+  last_name: "Yvett",
+  email: "sy@test.com"
+)
+
+Meeting.create(
+  title: "Best seminar for You",
+  agenda: "Self help, orginazation, life skills",
+  location:	"Tampa, FL"
+)
+
+Meeting.create(
+  title: "Learn to trim your pets claws",
+  agenda: "Calm them, trim them, give them a treat",
+  location:	"Sacremento, CA"
+)
