8beb3db620948eba171601120edf0af8116046e6
RobynSpaulding <robynm27@gmail.com>
2022-09-13 20:55:17 -0700

    adds speaker validation for first name, last name and email presence as well as email uniquiness

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,7 @@
 class Speaker < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniquiness: true 
   has_many :events
   has_many :meetings, through: :events 
 end
